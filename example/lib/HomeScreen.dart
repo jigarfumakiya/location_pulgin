243e8070c19bbecc828e180e9929174c3f6a6f74
Jigarfumakiya <jigarfumakiya@gmail.com>
2021-03-09 22:03:45 +0530

    Added mechanism to get user current location.
    Behind the scene, I am using a geolocator to get user location and communicating with location_plugin and the user can have position object.

--- example/lib/HomeScreen.dart
+++ example/lib/HomeScreen.dart
@@ -16,7 +16,21 @@ class _HomeScreenState extends State<HomeScreen> {
     showDialog(
       context: context,
       builder: (context) {
-        return Center(child: CircularProgressIndicator(strokeWidth: 5));
+        return AlertDialog(
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Making server request',
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+              SizedBox(height: 5),
+              CircularProgressIndicator(strokeWidth: 5),
+            ],
+          ),
+        );
       },
     );
     try {
@@ -44,26 +58,43 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  Future<void> getLocationPostRequest() async {
+  Future<void> getLocationPostRequest(BuildContext context) async {
     showDialog(
       context: context,
       builder: (context) {
-        return Center(child: CircularProgressIndicator(strokeWidth: 5));
+        return AlertDialog(
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(height: 5),
+              Text(
+                'Getting user location and Making server request',
+                style: TextStyle(fontSize: 16, color: Colors.black),
+              ),
+              SizedBox(height: 5),
+              CircularProgressIndicator(strokeWidth: 5),
+            ],
+          ),
+        );
       },
     );
+    String baseUrl = 'https://6045d210f0c6dc00177b0cba.mockapi.io/postGeoInfo';
 
-    //this methods will get user current location and post to server
     try {
-      String baseUrl =
-          'https://6045d210f0c6dc00177b0cba.mockapi.io/postGeoInfo';
+      //this methods will get user current location
+      Position _userPos = await _locationPlugin.getUserPosition();
+
       Map<String, dynamic> postData = new Map<String, dynamic>();
-      postData['lat'] = 22.2587;
-      postData['long'] = 71.1924;
-      postData['extra'] = 'Gujarat';
+      postData['lat'] = _userPos.latitude;
+      postData['long'] = _userPos.latitude;
+      postData['extra'] = _userPos.accuracy.toString();
 
       APIRequest apiRequest = new APIRequest(
           baseUrl: baseUrl, methods: RequestMethods.POST, data: postData);
 
+      //this methods will post user location to server.
       String response = await _locationPlugin.locationLog(request: apiRequest);
       setState(() {
         _response = response;
@@ -99,7 +130,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: getLocationPostRequest,
+              onPressed: () => getLocationPostRequest(context),
               child: Text('Get Location Log',
                   style: TextStyle(color: Colors.white)),
               style: ElevatedButton.styleFrom(
--- lib/location_plugin.dart
+++ lib/location_plugin.dart
@@ -1,14 +1,16 @@
 import 'dart:async';
 import 'package:flutter/services.dart';
-import 'package:location_permissions/location_permissions.dart';
+import 'package:geolocator/geolocator.dart';
+
 import 'package:location_plugin/network-layer/APIRequest.dart';
 export 'package:location_plugin/network-layer/APIRequest.dart';
+export 'package:geolocator_platform_interface/src/models/position.dart';
 
 class LocationPlugin {
   static const MethodChannel _channel = const MethodChannel('location_plugin');
-  LocationPermissions _locationPermissions = LocationPermissions();
 
   static Future<String> get platformVersion async {
+    ///this methods is just checking which android version running
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
@@ -21,47 +23,45 @@ class LocationPlugin {
     return location;
   }
 
-  Future<String> getUserLocationLog() async {
-    PermissionStatus status = await _requestPermission();
-    if (status == PermissionStatus.granted) {
-      try {
-        final String location = await _channel.invokeMethod('getCurrentLocation');
-        return location;
-      } catch (e) {
-        throw e;
+  Future<Position> getUserPosition({LocationAccuracy accuracy}) async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    /// check if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      /// Location services are not enabled don't continue
+      /// accessing the position and request users of the
+      /// App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        /// Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
       }
-    } else if (status == PermissionStatus.restricted) {
-      _locationPermissions.openAppSettings();
+
+      if (permission == LocationPermission.denied) {
+        /// Permissions are denied, next time you could try
+        /// requesting permissions again (this is also where
+        /// Android's shouldShowRequestPermissionRationale
+        /// returned true. According to Android guidelines
+        /// your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
     }
-  }
 
-  Future<PermissionStatus> _checkForPermission() async {
-    PermissionStatus permission =
-        await _locationPermissions.checkPermissionStatus();
-
-    //check the permission status
-    switch (permission) {
-      case PermissionStatus.unknown:
-        break;
-      case PermissionStatus.denied:
-        break;
-      case PermissionStatus.granted:
-        return permission;
-        break;
-      case PermissionStatus.restricted:
-        // TODO: Handle this case.
-
-        break;
+    /// When we reach here, permissions are granted and we can
+    /// continue accessing the position of the device.
+    try {
+      return await Geolocator.getCurrentPosition(
+          desiredAccuracy: accuracy ?? LocationAccuracy.best);
+    } catch (e) {
+      return Future.error(e);
     }
-  }
-
-  Future<PermissionStatus> _requestPermission(
-      {LocationPermissionLevel level =
-          LocationPermissionLevel.location}) async {
-    PermissionStatus permission =
-        await _locationPermissions.requestPermissions(permissionLevel: level);
-    print(permission.index);
-    print(permission);
-    return permission;
   }
 }
